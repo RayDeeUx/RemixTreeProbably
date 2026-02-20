7dfb5c5edcf45a5026c2d7efdd71f06f49a27a64
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-20 12:32:06 -0500

    ??????????

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -5,5 +5,4 @@ using namespace geode::prelude;
 using GDHistoryRemixTreeTask = geode::Task<geode::Result<std::string, std::string>>;
 
 namespace Utils {
-	GDHistoryRemixTreeTask fetchRemixesForLevelID();
 }
