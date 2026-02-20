95da458cc0d1a2e0bc004112fc9e7b7cc51864ec
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-20 12:46:28 -0500

    FUCKING HELL

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 
 class $modify(MyInfoLayer, InfoLayer) {
 	struct Fields {
-		async::TaskHolder<GDHistoryRemixTreeTask> listener {};
+		async::TaskHolder<WebResponse> listener {};
 	};
 	void onClose(CCObject* sender) {
 		Manager::get()->levelID = 0;
